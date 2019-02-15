1fd17c59260ef74a1ea7ffdcc7f753d0c5f6fccb
Deniz Ugurlu <mail@denizugurlu.de>
2019-02-15 13:26:26 +0100

    Add owners and vets page and controllers (closes #7)

---
+++ pet-clinic-web/src/main/java/de/denizugurlu/sfgpetclinic/controller/VetsController.java
@@ -0,0 +1,14 @@
+package de.denizugurlu.sfgpetclinic.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/vets")
+public class VetsController {
+
+    @RequestMapping({"", "/", "/index", "/index.html"})
+    public String listVets() {
+        return "vets/index";
+    }
+}
