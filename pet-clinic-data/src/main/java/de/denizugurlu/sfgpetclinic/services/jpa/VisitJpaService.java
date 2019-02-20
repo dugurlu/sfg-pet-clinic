b307e5eca4d91d18b2262578012e80f2ca3b0d17
Deniz Ugurlu <mail@denizugurlu.de>
2019-02-20 19:47:48 +0100

    Implement Visit JPA Service (closes #40).

---
+++ pet-clinic-data/src/main/java/de/denizugurlu/sfgpetclinic/services/jpa/VisitJpaService.java
@@ -0,0 +1,50 @@
+package de.denizugurlu.sfgpetclinic.services.jpa;
+
+import de.denizugurlu.sfgpetclinic.model.Visit;
+import de.denizugurlu.sfgpetclinic.repositories.VisitRepository;
+import de.denizugurlu.sfgpetclinic.services.VisitService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("jpa")
+public class VisitJpaService implements VisitService {
+
+    private final VisitRepository visitRepository;
+
+    public VisitJpaService(VisitRepository visitRepository) {
+        this.visitRepository = visitRepository;
+    }
+
+    @Override
+    public Set<Visit> findAll() {
+        Set<Visit> result = new HashSet<>();
+
+        visitRepository.findAll().forEach(result::add);
+
+        return result;
+    }
+
+    @Override
+    public Visit findById(Long aLong) {
+        return visitRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Visit save(Visit object) {
+        return visitRepository.save(object);
+    }
+
+    @Override
+    public void delete(Visit object) {
+        visitRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        visitRepository.deleteById(aLong);
+    }
+}
