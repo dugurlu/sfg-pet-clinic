34d0c582a951c3121ae65f7bf8d918a4e3956552
Deniz Ugurlu <mail@denizugurlu.de>
2019-02-20 19:45:38 +0100

    Implement Visit Map Service (closes #39)

---
+++ pet-clinic-data/src/main/java/de/denizugurlu/sfgpetclinic/services/VisitService.java
@@ -0,0 +1,6 @@
+package de.denizugurlu.sfgpetclinic.services;
+
+import de.denizugurlu.sfgpetclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
---
+++ pet-clinic-data/src/main/java/de/denizugurlu/sfgpetclinic/services/map/VisitMapService.java
@@ -0,0 +1,38 @@
+package de.denizugurlu.sfgpetclinic.services.map;
+
+import de.denizugurlu.sfgpetclinic.model.Visit;
+import de.denizugurlu.sfgpetclinic.services.VisitService;
+
+import java.util.Set;
+
+public class VisitMapService extends AbstractMapService<Visit, Long> implements VisitService {
+
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Visit findById(Long aLong) {
+        return super.findById(aLong);
+    }
+
+    @Override
+    public Visit save(Visit visit) {
+        if(visit.getPet() == null || visit.getPet().getOwner() == null || visit.getPet().getId() == null) {
+            throw new RuntimeException("Invalid Visit");
+        }
+
+        return super.save(visit);
+    }
+
+    @Override
+    public void delete(Visit object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        super.deleteById(aLong);
+    }
+}
