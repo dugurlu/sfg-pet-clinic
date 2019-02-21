731f1e062697f7db1f5ba8fec38bf6953711346b
Deniz Ugurlu <mail@denizugurlu.de>
2019-02-21 18:43:29 +0100

    Add project Lombok (closes #44)

--- pet-clinic-data/src/main/java/de/denizugurlu/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/de/denizugurlu/sfgpetclinic/model/Vet.java
@@ -1,9 +1,16 @@
 package de.denizugurlu.sfgpetclinic.model;
 
+import lombok.*;
+
 import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "vet")
 public class Vet extends Person {
@@ -11,12 +18,4 @@ public class Vet extends Person {
     @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(name = "vet_specialties", joinColumns = @JoinColumn(name = "vet_id"), inverseJoinColumns = @JoinColumn(name = "specialty_id"))
     private Set<Specialty> specialties = new HashSet<>();
-
-    public Set<Specialty> getSpecialties() {
-        return specialties;
-    }
-
-    public void setSpecialties(Set<Specialty> specialties) {
-        this.specialties = specialties;
-    }
 }
