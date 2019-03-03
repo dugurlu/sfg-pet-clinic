60f27c0c6a537cfa6c903dbc45085c97c50ae93a
Deniz Ugurlu <mail@denizugurlu.de>
2019-03-03 19:57:17 +0100

    Implement Vets JSON Endpoint (closes #61).

--- pet-clinic-web/src/main/java/de/denizugurlu/sfgpetclinic/controller/VetsController.java
+++ pet-clinic-web/src/main/java/de/denizugurlu/sfgpetclinic/controller/VetsController.java
@@ -1,9 +1,13 @@
 package de.denizugurlu.sfgpetclinic.controller;
 
+import de.denizugurlu.sfgpetclinic.model.Vet;
 import de.denizugurlu.sfgpetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Set;
 
 @Controller
 public class VetsController {
@@ -20,5 +24,10 @@ public class VetsController {
         model.addAttribute("vets", vetService.findAll());
 
         return "vets/index";
+    }
+
+    @RequestMapping("/api/vets")
+    public @ResponseBody Set<Vet> getVetsJson() {
+        return vetService.findAll();
     }
 }
